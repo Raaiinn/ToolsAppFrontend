b7cb46f2763334650720e52d41569511822de1d7
Raaiinn <73810142+Raaiinn@users.noreply.github.com>
2023-05-24 23:59:51 -0500

    Delete src directory

--- src/app/filters/filter-by-brand.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByBrandPipe } from './filter-by-brand.pipe';
-
-describe('FilterByBrandPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByBrandPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
